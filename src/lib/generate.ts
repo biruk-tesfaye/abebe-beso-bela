53dfb1698d649201817a65819255884b6b220c12
Biruk Tesfaye <biruktesfayeve@gmail.com>
2022-04-12 19:24:05 +0300

    fix sentence and word range issues

--- src/lib/generate.ts
+++ src/lib/generate.ts
@@ -5,28 +5,46 @@ type IGenerate = {
   scope?: scopeType
   startWith?: boolean
 }
-
-const MAX_WORDS_PER_SENTENCE = 15
-const MIN_WORDS_PER_SENTENCE = 5
-
-const MAX_SENTENCES_PER_PARAGRAPH = 10
-const MIN_SENTENCES_PER_PARAGRAPH = 3
-
-export function generate(body: IGenerate = {}) {
-  const { amount = 1, scope = 'PARAGRAPHS', startWith = true } = body
+type IExtra = {
+  MIN_WORDS_PER_SENTENCE?: number
+  MAX_WORDS_PER_SENTENCE?: number
+  MIN_SENTENCES_PER_PARAGRAPH?: number
+  MAX_SENTENCES_PER_PARAGRAPH?: number
+}
+export function generate(options: IGenerate = {}, extra?: IExtra) {
+  const { amount = 1, scope = 'PARAGRAPHS', startWith = true } = options
+  let {
+    MAX_SENTENCES_PER_PARAGRAPH = 10,
+    MIN_SENTENCES_PER_PARAGRAPH = 3,
+    MIN_WORDS_PER_SENTENCE = 5,
+    MAX_WORDS_PER_SENTENCE = 15,
+  } = extra || {}
   let generated = []
+
+  MAX_WORDS_PER_SENTENCE = startWith
+    ? MAX_WORDS_PER_SENTENCE - 3
+    : MAX_WORDS_PER_SENTENCE
 
   //  if scope is paragraph then generated will contain amount of paragraphs
   if (scope === 'PARAGRAPHS') {
     for (let i = 0; i < amount; i++) {
-      generated.push(`${generateParagraph()} `)
+      generated.push(
+        `${generateParagraph(
+          MAX_SENTENCES_PER_PARAGRAPH,
+          MIN_SENTENCES_PER_PARAGRAPH,
+          MAX_WORDS_PER_SENTENCE,
+          MIN_WORDS_PER_SENTENCE
+        )}`
+      )
     }
   }
 
   // if scope is sentence then generated will contain amount of sentences
   if (scope === 'SENTENCES') {
     for (let i = 0; i < amount; i++) {
-      generated.push(`${generateSentence()} `)
+      generated.push(
+        `${generateSentence(MAX_WORDS_PER_SENTENCE, MIN_WORDS_PER_SENTENCE)}`
+      )
     }
   }
   // if scope is word then generated will contain amount of words
@@ -42,7 +60,12 @@ export function generate(body: IGenerate = {}) {
   return generated
 }
 
-function generateParagraph() {
+function generateParagraph(
+  MIN_SENTENCES_PER_PARAGRAPH: number,
+  MAX_SENTENCES_PER_PARAGRAPH: number,
+  MAX_WORDS_PER_SENTENCE: number,
+  MIN_WORDS_PER_SENTENCE: number
+) {
   const paragraph = []
   const sentence = getRandomNumber(
     MIN_SENTENCES_PER_PARAGRAPH,
@@ -50,12 +73,17 @@ function generateParagraph() {
   )
 
   for (let i = 0; i < sentence; i++) {
-    paragraph.push(`${generateSentence()}`)
+    paragraph.push(
+      `${generateSentence(MAX_WORDS_PER_SENTENCE, MIN_WORDS_PER_SENTENCE)}`
+    )
   }
-  return paragraph.join('\n')
+  return paragraph.join(' ')
 }
 
-function generateSentence() {
+function generateSentence(
+  MAX_WORDS_PER_SENTENCE: number,
+  MIN_WORDS_PER_SENTENCE: number
+) {
   const sentence = []
   const word = getRandomNumber(MIN_WORDS_PER_SENTENCE, MAX_WORDS_PER_SENTENCE)
 
@@ -72,5 +100,7 @@ function generateSentence() {
 }
 
 function getRandomNumber(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1) + min) //The maximum is inclusive and the minimum is inclusive
 }
--- test/generate.test.ts
+++ test/generate.test.ts
@@ -1,7 +1,15 @@
 import { generate } from '../dist/lib/generate'
-
+import { log } from 'console'
 describe('generate', () => {
   it('should generate a random text', () => {
-    expect(generate()).toHaveLength(1)
+    let generated = generate({
+      amount: 1,
+      scope: 'PARAGRAPHS',
+    })[0]
+    log(JSON.stringify(generated))
+    let g = generated.split('።')
+    log(g)
+    expect(g.length).toBeGreaterThanOrEqual(3)
+    expect(g.length).toBeLessThanOrEqual(10)
   })
 })
