68a901b60227eec889270ddad8b2aeac0d799900
Biruk Tesfaye <biruktesfayeve@gmail.com>
2022-04-12 15:01:53 +0300

    remove test file

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { generate } from './index.js'
-
-generate({
-  amount: 10,
-  scope: 'PARAGRAPHS',
-  startWith: true,
-})
-  .then((generated) => console.info(generated.join('\n')))
-  .catch(console.error)
