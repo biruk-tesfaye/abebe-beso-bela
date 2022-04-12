0a241008ffcc30bd22e41b9f665c2bcf8355e30b
Biruk Tesfaye <biruktesfayeve@gmail.com>
2022-04-12 16:09:09 +0300

    update test case

--- test/generate.test.ts
+++ test/generate.test.ts
@@ -2,6 +2,6 @@ import { generate } from '../dist/lib/generate'
 
 describe('generate', () => {
   it('should generate a random text', () => {
-    expect(generate()).resolves.toHaveLength(2)
+    expect(generate()).toHaveLength(1)
   })
 })
